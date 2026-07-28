7ffca85c362a9f10a7832813b5dd7a576c96a7aa
NathanBoddie <137906221+NathanBoddie@users.noreply.github.com>
2026-07-27 21:07:19 -0400

    Refactor isp.py to use command line arguments

--- isp.py
+++ isp.py
@@ -1,14 +1,31 @@
 #Inter-Simulation Processing between the output file of QE to QE input
 
-intial = open("in.txt","r")
-post=open("out.txt","w")
+import sys
 
+intial = open(sys.argv[1],"r")
 
 cont=intial.read()
 cont=cont.split("\n")
 
 cont.index("   JOB DONE.")
 
+#preparing output file
+
+path=sys.argv[1].split("/")[0:-1]
+path="/".join(path)+"/"
+
+pos = cont.index("     Current dimensions of program PWSCF are:")
+current_md="".join(filter(str.isdigit, cont[pos-2].split()[-1]))
+if current_md=="": current_md = "0"
+
+next_md=int(current_md)+1
+
+post=open(path+"md"+str(next_md)+".in","w")
+
+config=open("md-config.txt","r")
+post.write(config.read())
+
+#processing last coords. 
 pos =  cont.index("     The maximum number of steps has been reached.")
 
 
@@ -25,12 +42,13 @@ for i in range(90):
     temp_cont.append(cont[pos-102+i])
 cont=temp_cont
 
+
 for i in range(len(cont)):
     x=cont[i].split()
     if len(x) == 4:
         cont[i]+=("    1   1   1")
     post.write(cont[i]+"\n")
 
-print("completed")
+post.write("K_POINTS gamma")
 
-#expend in order to take .out file and plug into .in than modify to add it into sbatch
+print("completed")
